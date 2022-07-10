88074064bf6830561827ebc0412f52a986e4e023
D:\works <masu11108@icloud.com>
2022-07-10 16:05:44 +0900

    :sparkles: add sample page

--- src/assets/scripts/modules/carousel.ts
+++ src/assets/scripts/modules/carousel.ts
@@ -9,20 +9,25 @@ const topCarousel = new Splide('.top-carousel', {
     resetProgress : false,
     pauseOnFocus : false,
     pauseOnHover : false,
+    arrows: false,
     keyboard : 'global',
     pagination : true,
     rewind : true,
     speed : 1000,
     perPage : 1,
     classes : {
-		pagination : 'splide__pagination carousel-pagination__list',
-		page : 'splide__pagination__page carousel-pagination__dots',
+        arrows: 'splide__arrows top-carousel-arrow',
+		arrow : 'splide__arrow top-carousel-arrow__item',
+		prev  : 'splide__arrow--prev top-carousel-arrow__item--prev',
+		next  : 'splide__arrow--next top-carousel-arrow__item--next',
+		pagination : 'splide__pagination top-carousel-pagination__list',
+		page : 'splide__pagination__page top-carousel-pagination__bullet',
     }
 });
 
 topCarousel.on('pagination:mounted', data => {
     data.items.forEach(item => {
-        item.li.classList.add('carousel-pagination__item');
+        item.li.classList.add('top-carousel-pagination__item');
     });
 });
 
